60dea77f6be21143df33ab001c323479a8ab7b98
Shivangi Kumari <12shivangikumari@gmail.com>
2025-02-26 14:03:23 +0530

    initial commit no responsivesness

--- app/components/section1.tsx
+++ app/components/section1.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight } from 'lucide-react'; 
 
 export default function LandingPage() {
   return (
